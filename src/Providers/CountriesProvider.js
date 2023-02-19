0c8a85d35658f96bfebe8677d88c75314fb87c3f
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-02-19 12:35:16 +0330

    add countries provider

---
+++ src/Providers/CountriesProvider.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useContext, useCallback } from 'react';
+
+const CountryContext = React.createContext({});
+
+export const CountriesProvider = ({ children }) => {
+  const [countries, setCountries] = useState(null);
+  const [state, setState] = useState('loading');
+
+  const findCountryByName = useCallback(
+    (name) => {
+      if (!countries) return;
+      return countries.find((country) => country.name.common === name);
+    },
+    [countries]
+  );
+
+  const findByAltSpelling = (altSpelling) => {
+    if (!countries) return;
+    return countries.find((country) => country.cca3 === altSpelling);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          'https://restcountries.com/v3.1/all'
+        );
+        const data = await response.json();
+
+        const sorted = data.sort((a, b) => b.population - a.population);
+        setCountries(sorted);
+        setState('resolved');
+      } catch (err) {
+        setState('error');
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  return <CountryContext.Provider value={{ countries, findCountryByName, findByAltSpelling, state }}>{children}</CountryContext.Provider>;
+};
+
+export const useCountries = () => {
+  const { countries, findCountryByName, findByAltSpelling, state } = useContext(CountryContext);
+
+  return {
+    countries,
+    findCountryByName,
+    findByAltSpelling,
+    state,
+  };
+};
