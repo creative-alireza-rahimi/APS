0258314e2d90a62ce8e9c453b44ad953f08d8262
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-10 00:20:36 +0330

    add header

---
+++ src/components/page/header/Header.jsx
@@ -0,0 +1,11 @@
+import { Title, Head } from "./Header.style";
+
+export const Header = () => {
+    return (
+        <>
+            <Head>
+                <Title>To Do List</Title>
+            </Head>
+        </>
+    );
+}
---
+++ src/components/page/header/Header.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Head = styled.section`
+    border-bottom: 1px solid black;
+    display: flex;
+    flex-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.p`
+    font-size: 1.2rem;
+    font-weight: bolder;
+    margin: 0.8rem 0;
+    color: rgba(0, 0, 0, 0.8);
+`;
