0714fdf06d19affdf602ec2ba0f77395964b58c1
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-10 00:20:22 +0330

    add footer

---
+++ src/components/page/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import { Title, Foot, Link } from "./Footer.style";
+
+export const Footer = () => {
+    return (
+        <>
+            <Foot>
+                <Title>
+                    <Link href="https://t.me/creativealireza">
+                        @creativealireza
+                    </Link>
+                </Title>
+            </Foot>
+        </>
+    );
+}
---
+++ src/components/page/footer/Footer.style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Foot = styled.section`
+    border-top: 1px solid black;
+    display: flex;
+    flex-items: center;
+    justify-content: center;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+`;
+
+export const Title = styled.p`
+    font-size: 1.2rem;
+    font-weight: bolder;
+    margin: 0.8rem 0;
+    color: rgba(0, 0, 0, 0.8);
+    cursor: pointer;
+`;
+
+export const Link = styled.a`
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.8);
+`;
