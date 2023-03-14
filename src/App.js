7b084ed83e030ee7f2d66ec2774582bb992f1710
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-14 12:55:20 +0330

    add Header and Footer to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
+
 export default function App() {
   return (
     <>
+      <Header />
+     
+      <Footer />
     </>
   );
 }
