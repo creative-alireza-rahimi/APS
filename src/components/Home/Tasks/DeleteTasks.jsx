7e536824ba1e94892e9ad09b606c038c56e506e8
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-20 22:21:02 +0330

    add DeleteTasks

---
+++ src/components/Home/Tasks/DeleteTasks.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Button, Stack } from '@mui/material';
+import { DeleteDialog } from "./DeleteDialog/DeleteDialog";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export const DeleteTasks = ({ title }) => {
+  const [openDelete, setOpenDelete] = useState(false);
+
+  function handleDeleteDialog() {
+    setOpenDelete(openDelete => !openDelete)
+}
+  return (
+    <Stack alignSelf="flex-start">
+      <Button
+        disableRipple
+        variant="contained"
+        color="error"
+        startIcon={<DeleteIcon />}
+        sx={{
+          textTransform: "none",
+        }}
+        onClick={handleDeleteDialog}
+      >
+        {title}
+      </Button>
+      <DeleteDialog 
+        modalStatus={openDelete} 
+        handleDeleteDialog={handleDeleteDialog} 
+        DeleteDialogTitle="Delete All Tasks?"
+        DialogDescription="Are You Sure? Sure Sure?"
+        DeleteBtnTitle="Delete All"
+        CancelBtnTitle="Mission Abort" />
+
+    </Stack>
+  );
+}
