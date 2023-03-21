ccaeeb887eb39f94bde9e5bed250325ca7d14e72
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-21 10:57:49 +0330

    add NavLink to UITabs

--- src/components/UITabs/UITabs.jsx
+++ src/components/UITabs/UITabs.jsx
@@ -1,9 +1,10 @@
 import { Members } from '../Members/Members';
 import { Home } from '../Home/Home';
 import { History } from '../History/History';
-import {useState} from "react";
-import {Tab, Box} from "@mui/material";
+import { useState } from "react";
+import { Tab, Box } from "@mui/material";
 import { TabPanel, TabContext, TabList } from '@mui/lab';
+import { NavLink } from "react-router-dom";
 
 export const UITabs = () => {
   const [value, setValue] = useState("1");
@@ -13,15 +14,16 @@ export const UITabs = () => {
   };
 
   return (
-    <Box sx={{ width: '100%'}}>
+    <Box sx={{ width: '100%' }}>
       <TabContext value={value} >
         <Box sx={{ width: '100%' }}>
           <TabList onChange={handleChange} aria-label="list of Tabs" centered>
-            <Tab label="Home" value="1" />
-            <Tab label="History" value="2" />
-            <Tab label="Members" value="3" />
+            <Tab label="Home" value="1" component={NavLink} to={"/home"} />
+            <Tab label="History" value="2" component={NavLink} to={"/history"} />
+            <Tab label="Members" value="3" component={NavLink} to={"/members"} />
           </TabList>
         </Box>
+        
         <TabPanel value="1">{<Home />}</TabPanel>
         <TabPanel value="2">{<History />}</TabPanel>
         <TabPanel value="3">{<Members />}</TabPanel>
