860d6cd1c4e167c629f92a2081476dfc7df56519
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-21 10:57:09 +0330

    add NavLink to Profile

--- src/components/Header/ProfileNav/ProfileNav.jsx
+++ src/components/Header/ProfileNav/ProfileNav.jsx
@@ -1,20 +1,23 @@
 import PersonIcon from '@mui/icons-material/Person';
 import { IconButton } from '@mui/material';
+import { NavLink } from "react-router-dom";
 
 export const ProfileNav = () => {
     return (
-        <IconButton disableRipple color="primary" aria-label="profile picture" component="label">
-            <PersonIcon
-                
-                sx={{
-                    color: 'white',
-                    margin: "0 0.6rem",
-                    
-                    "&:hover": {
-                        backgroundColor: "transparent"
-                    },
-                }}
-            />
-        </IconButton>
+        <NavLink to={'/profile'}>
+            <IconButton disableRipple color="primary" aria-label="profile picture" component="label">
+                <PersonIcon
+
+                    sx={{
+                        color: 'white',
+                        margin: "0 0.6rem",
+
+                        "&:hover": {
+                            backgroundColor: "transparent"
+                        },
+                    }}
+                />
+            </IconButton>
+        </NavLink>
     );
 }
