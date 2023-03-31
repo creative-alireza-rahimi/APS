d21af298101be542c7de0d933ef4641c3328c2da
Mamad Mamadian <tocivey603@mustbeit.com>
2023-03-31 23:39:00 +0430

    add getAdmin

--- src/API/API.js
+++ src/API/API.js
@@ -2,10 +2,18 @@ import axios from "axios";
 
 const COUNTRY_URL = `https://restcountries.com/v3.1/all`;
 
+export async function getAllLanguages() {
+    try {
+        const response = await axios.get(COUNTRY_URL);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export async function newAdmin(admin) {
     try {
         const response = await axios.post("http://localhost:5000/admin/newAdmin", admin);
-        console.log(response);
         return response;
     } catch (error) {
         console.error(error);
@@ -16,19 +24,17 @@ export async function newMembers(member) {
     try {
         console.log(member);
         const response = await axios.put("http://localhost:5000/admin/newMember", member);
-        console.log(response);
         return response;
     } catch (error) {
         console.error(error);
     }
 }
 
-export async function getAllLanguages() {
+export async function getAdmin({ email, password }) {
     try {
-        const response = await axios.get(COUNTRY_URL);
-        return response.data;
+        const response = await axios.post("http://localhost:5000/login/getAdmin", { email, password });
+        return response;
     } catch (error) {
         console.error(error);
     }
 }
-
