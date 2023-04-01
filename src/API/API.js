43ba61516d7e622fbc4af103cbf4731966899328
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-02 00:56:45 +0430

    add newTask

--- src/API/API.js
+++ src/API/API.js
@@ -11,6 +11,7 @@ export async function getAllLanguages() {
     }
 }
 
+// Admins
 export async function newAdmin(admin) {
     try {
         const response = await axios.post("http://localhost:5000/admin/newAdmin", admin);
@@ -33,6 +34,19 @@ export async function newMembers(member) {
 export async function getAdmin({ email, password }) {
     try {
         const response = await axios.post("http://localhost:5000/login/getAdmin", { email, password });
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Tasks
+export async function newTask(task) {
+    try {
+        console.log(task);
+        const response = await axios.post("http://localhost:5000/tasks/newTask", task);
+        console.log("response: ", response);
+
         return response;
     } catch (error) {
         console.error(error);
