a42917c6a3ced5176cd9928292955fb575d0ca91
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-03 12:59:15 +0430

    add members, tasks and total tasks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Tasks } from "./Tasks/Tasks";
 import { DeleteTasks } from "./Tasks/DeleteTasks";
 import { AddTasks } from "./Tasks/AddTasks/AddTasks";
 import { TotalTasks } from "./Tasks/TotalTasks";
+import { useSelector } from 'react-redux';
+import { getTasks } from "../../API/API";
 import {
     CssBaseline,
     Container,
@@ -13,20 +15,39 @@ import {
 } from '@mui/material';
 
 export const Home = () => {
+    const members = useSelector(state => state.members)
     const [hasComplete, setHasComplete] = useState(true)
     const [openAdd, setOpenAdd] = useState(false);
+    const [tasks, setTasks] = useState([])
+    const { adminId } = members?.at(0);
 
     function handleAddDialog() {
         setOpenAdd(openAdd => !openAdd)
     }
+
+    useEffect(() => {
+        getTasks({ adminId })
+            .then(tasks => setTasks(tasks.data))
+    }, [adminId, openAdd])
 
     return (
         <>
             <CssBaseline />
             <Container>
                 <Stack justifyContent="space-between" alignItems="center" spacing={3}>
-                    <Stack direction="row" flexWrap="wrap">
-                        <Avatar alt="Alireza" src="src" />
+                    <Stack direction="row" flexWrap="wrap" alignItems="center">
+                        {members?.at(0)?.members?.map(member =>
+                            <Avatar
+                                key={member?.memberId}
+                                alt={member?.fullName}
+                                src={member?.profilePhoto}
+                                sx={{
+                                    margin: "0.2rem 0.4rem",
+                                    minWidth: member?.isAdmin && "3rem",
+                                    minHeight: member?.isAdmin && "3rem",
+                                    border: member?.isAdmin && `3px solid #388e3c`,
+                                }}
+                            />)}
                     </Stack>
 
                     <Divider variant="middle" role="tasks" sx={{ width: "100%" }}>
@@ -38,10 +59,10 @@ export const Home = () => {
                             <AddTasks openAdd={openAdd} handleAddDialog={handleAddDialog} />
                             <DeleteTasks title="Tasks" />
                         </Stack>
-                        <TotalTasks />
+                        <TotalTasks total={tasks?.length} />
                     </Stack>
 
-                    <Tasks edit />
+                    <Tasks edit tasks={tasks} updateTasks={setTasks} adminId={adminId} />
 
                     {hasComplete &&
                         <>
