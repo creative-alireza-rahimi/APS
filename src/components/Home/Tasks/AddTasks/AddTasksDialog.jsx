9909be3a05f3f7819ae1ba8dd1ec7b05e83714a1
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-03 12:52:44 +0430

    add LinearProgress, SubmitTask & DialogContent

--- src/components/Home/Tasks/AddTasks/AddTasksDialog.jsx
+++ src/components/Home/Tasks/AddTasks/AddTasksDialog.jsx
@@ -1,17 +1,55 @@
-import { TasksForm } from "./TasksForm";
+import { useState } from 'react';
+import { AddTextarea } from "./AddTextarea";
+import { AddMembers } from "./AddMembers";
+import { newTask } from "../../../../API/API";
+import { useSelector } from 'react-redux';
 import {
     Button,
     Dialog,
     DialogActions,
     DialogContent,
     DialogTitle,
-    Divider
+    Divider,
+    TextField,
+    LinearProgress
 } from '@mui/material';
 
 export const AddTasksDialog = ({ modalStatus, handleAddDialog }) => {
+    const taskMembers = useSelector(state => state.members)
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [selectedMembers, setSelectedMembers] = useState([]);
+
+    function handleTitle(e) {
+        setTitle(e.target.value)
+    }
+
+    function handleDescription(des) {
+        setDescription(des)
+    }
+
+    async function handleSubmitTask() {
+        setIsLoading(true)
+
+        const task = await newTask({
+            title,
+            description,
+            members: selectedMembers,
+            adminId: taskMembers?.at(0)?.adminId,
+            isCompleted: false,
+            isEdited: false,
+            isDeleted: false,
+        })
+
+        if (task.status === 200) {
+            setIsLoading(false)
+            handleAdd();
+        }
+    }
 
     function handleAdd() {
-        console.log("add");
         handleAddDialog()
     }
 
@@ -22,6 +60,7 @@ export const AddTasksDialog = ({ modalStatus, handleAddDialog }) => {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
         >
+            {isLoading && <LinearProgress />}
             <DialogTitle id="alert-dialog-title">
                 Add a Task
             </DialogTitle>
@@ -29,11 +68,27 @@ export const AddTasksDialog = ({ modalStatus, handleAddDialog }) => {
             <Divider variant="middle" />
 
             <DialogContent>
-                <TasksForm />
+                <TextField
+                    autoFocus
+                    id="title"
+                    label="Title"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleTitle}
+                />
+
+                <AddTextarea setDescription={handleDescription} />
+
+                <AddMembers
+                    members={taskMembers?.at(0)?.members}
+                    selectMembers={{ value: selectedMembers, setValue: setSelectedMembers }} />
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleAddDialog}>Cancel</Button>
-                <Button onClick={handleAdd} autoFocus>
+                <Button onClick={() => {
+                    handleSubmitTask();
+                }} autoFocus>
                     Add
                 </Button>
             </DialogActions>
