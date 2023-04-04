8c23ce9248cd38600c7e60d117b369b3facdc8ea
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-04 20:57:42 +0330

    add filter isDeleted to getTasks

--- controller/taskController.js
+++ controller/taskController.js
@@ -12,17 +12,22 @@ const getTasks = async (req, res) => {
 
     if (!admin) return res.status(204).json({ message: "No admin found" });
 
-    const tasksArray = admin?.tasks?.map(task => ({
-      title: task?.title,
-      description: task?.title,
-      members: task?.members,
-      isCompleted: task?.isCompleted,
-      isEdited: task?.isEdited,
-      isDeleted: task?.isDeleted,
-      taskId: task?._id.toString()
-    }))
+    const tasks = [];
+    admin?.tasks?.map(task => {
+      if (!task?.isDeleted) {
+        tasks.push({
+          title: task?.title,
+          description: task?.description,
+          members: task?.members,
+          isCompleted: task?.isCompleted,
+          isEdited: task?.isEdited,
+          isDeleted: task?.isDeleted,
+          taskId: task?._id.toString()
+        })
+      }
+    })
 
-    res.json(tasksArray);
+    res.json(tasks);
   } catch (err) {
     console.log(err);
   }
@@ -60,29 +65,7 @@ const createTask = async (req, res) => {
   }
 };
 
-// @desc - edit a task
-// @route - PUT '/'
-// @access - public
-const editTask = async (req, res) => {
-  const { id, title, describe, userId } = req?.body;
-  if (!id) return res.status(400).json({ message: "Id parameter is required" });
 
-  try {
-    const task = await Task.findOne({ _id: id });
-    if (!task)
-      return res.status(204).json({ message: `No matches task with id:${id}` });
-
-    if (req.body?.title) task.title = title;
-    if (req.body?.describe) task.describe = describe;
-    if (req.body?.userId) task.userId = userId;
-
-    await task.save();
-    const allTasks = await Task.find();
-    res.json(allTasks);
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 // @desc - complete a task
 // @route - PUT '/'
@@ -108,23 +91,5 @@ const completeTask = async (req, res) => {
   }
 };
 
-// @desc - delete a task
-// @route - DELETE '/'
-// @access - public
-const deleteTask = async (req, res) => {
-  const { id } = req?.body;
-  if (!id) return res.status(400).json({ message: "id parameter is required" });
-
-  try {
-    const task = Task.findOne({ _id: id });
-    if (!task)
-      return res.status(204).json({ message: `no matches task with id:${id}` });
-    await task.deleteOne();
-    const allTasks = await Task.find();
-    res.json(allTasks);
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 module.exports = { getTasks, createTask, deleteTask, completeTask, editTask };
