ab98c49a22815fd40ced86ec8f4876e7324616cc
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-06 22:15:11 +0330

    upgrade delete all tasks

--- src/components/Home/Tasks/DeleteTasks.jsx
+++ src/components/Home/Tasks/DeleteTasks.jsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
-import { Button, Stack } from '@mui/material';
-import { DeleteDialog } from "./DeleteDialog/DeleteDialog";
+import {
+  Stack,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  LinearProgress
+} from '@mui/material';
+import { deleteAllTasks } from "../../../API/API";
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export const DeleteTasks = ({ title }) => {
+export const DeleteTasks = ({ title, adminId, isReq }) => {
   const [openDelete, setOpenDelete] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function handleDeleteDialog() {
-    setOpenDelete(openDelete => !openDelete)
-}
+  async function handleDeleteDialog() {
+    setOpenDelete(openDelete => !openDelete);
+  }
+
+  async function handleDeleteAll() {
+    setIsLoading(true)
+
+    const deleteTasks = await deleteAllTasks({ adminId });
+
+    if (deleteTasks?.status === 200) {
+      handleDeleteDialog();
+      setIsLoading(false)
+      isReq(req => !req);
+    }
+  }
+
   return (
     <Stack alignSelf="flex-start">
       <Button
@@ -23,14 +47,31 @@ export const DeleteTasks = ({ title }) => {
       >
         {title}
       </Button>
-      <DeleteDialog 
-        modalStatus={openDelete} 
-        handleDeleteDialog={handleDeleteDialog} 
-        DeleteDialogTitle={`Delete ${title}`}
-        DialogDescription="Are You Sure? Sure Sure?"
-        DeleteBtnTitle="Delete All"
-        CancelBtnTitle="Mission Abort" />
+      <Dialog
+        open={openDelete}
+        onClose={handleDeleteDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {isLoading && <LinearProgress />}
+        <DialogTitle id="alert-dialog-title">
+          Delete All Tasks?
+        </DialogTitle>
 
+        <Divider variant="middle" />
+
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {"Are You Sure? Sure Sure?"}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteDialog}> {"Mission Abort"}</Button>
+          <Button onClick={handleDeleteAll}>
+            {"Delete All"}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Stack>
   );
 }
