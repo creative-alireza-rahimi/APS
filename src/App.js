1e4776d416c4332013a7637737a625669e2a0261
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-07 22:41:34 +0330

    add auth handler and UITabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Header } from "./components/Header/Header"
 import { Footer } from "./components/Footer/Footer"
+import { UITabs } from "./components/UITabs/UITabs";
+import { Authentication } from "./Authentication/Authentication";
+import { useSelector } from 'react-redux'
 
 export default function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth)
+
   return (
     <>
       <Header />
-     
+
+      {isAuth ? <UITabs /> : <Authentication isAuth />}
+
       <Footer />
     </>
   );
