5e76b6086ae438cd9f3dbeff88225adbab45f363
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-07 21:10:35 +0330

    filter Member page by isAdmin

--- src/components/UITabs/UITabs.jsx
+++ src/components/UITabs/UITabs.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Tab, Box } from "@mui/material";
 import { TabPanel, TabContext, TabList } from '@mui/lab';
 import { NavLink } from "react-router-dom";
+import { readData } from "../../Tools/localActions";
 
 export const UITabs = () => {
   const [value, setValue] = useState("1");
@@ -13,6 +14,8 @@ export const UITabs = () => {
     setValue(newValue);
   };
 
+  const user = readData("user");
+
   return (
     <Box sx={{ width: '100%' }}>
       <TabContext value={value} >
@@ -20,10 +23,10 @@ export const UITabs = () => {
           <TabList onChange={handleChange} aria-label="list of Tabs" centered>
             <Tab label="Home" value="1" component={NavLink} to={"/home"} />
             <Tab label="History" value="2" component={NavLink} to={"/history"} />
-            <Tab label="Members" value="3" component={NavLink} to={"/members"} />
+            {user?.isAdmin && <Tab label="Members" value="3" component={NavLink} to={"/members"} />}
           </TabList>
         </Box>
-        
+
         <TabPanel value="1">{<Home />}</TabPanel>
         <TabPanel value="2">{<History />}</TabPanel>
         <TabPanel value="3">{<Members />}</TabPanel>
