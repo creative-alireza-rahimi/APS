358fa50a7f1e69c4053bb37c503e04ac93c05b7b
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-07 17:57:10 +0330

    upgrade LoginVisitor

--- src/Authentication/Login/LoginVisitor.jsx
+++ src/Authentication/Login/LoginVisitor.jsx
@@ -1,22 +1,46 @@
-
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from 'react-router-dom';
+import { getVisitor } from "../../API/API";
+import { useDispatch } from 'react-redux';
+import { isAuth } from '../authSlice';
+import { getMember } from '../../components/Members/MembersSlice';
+import { saveData } from '../../Tools/saveToLocal';
 import {
     Button,
     Dialog,
     DialogActions,
     DialogContent,
-    DialogContentText,
     DialogTitle,
-    TextField
+    TextField,
+    LinearProgress,
+    useMediaQuery
 } from '@mui/material';
 
 export const LoginVisitor = ({ openForm, OpenLogin, AuthDialog }) => {
-    const [filedError, setFiledError] = useState(false);
+    const formMinWidth = useMediaQuery('(max-width : 420px)');
 
-    const handleFillInput = (e) => {
-        e.target.value ? setFiledError(false) : setFiledError(true);
-    };
+    const [username, setUsername] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const dispatch = useDispatch()
+
+    const handleLoginVisitor = async () => {
+        setIsLoading(true)
+
+        const visitor = await getVisitor({ username })
+
+        if (visitor.status === 200) {
+            dispatch(isAuth())
+            setIsLoading(false)
+
+            saveData("user", visitor.data)
+            dispatch(getMember(visitor.data))
+        }
+    }
+
+    function handleForm(e) {
+        setUsername(e?.target?.value)
+    }
 
     return (
         <Dialog
@@ -27,23 +51,21 @@ export const LoginVisitor = ({ openForm, OpenLogin, AuthDialog }) => {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
         >
+            {isLoading && <LinearProgress />}
             <DialogTitle id="alert-dialog-title">
                 Login (Visitor)
             </DialogTitle>
             <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Enter your Github or LinkedIn Username to Proceed.
-                </DialogContentText>
                 <TextField
                     required
-                    error={filedError}
+                    error={!username}
                     helperText="Fill the input."
                     id="username"
                     label="Username"
                     variant="standard"
                     autoFocus
-                    sx={{ marginTop: "1rem", minWidth: "100%" }}
-                    onChange={handleFillInput}
+                    sx={{ marginTop: "1rem", minWidth: formMinWidth ? "100%" : 300 }}
+                    onChange={handleForm}
                 />
             </DialogContent>
             <DialogActions>
@@ -56,7 +78,9 @@ export const LoginVisitor = ({ openForm, OpenLogin, AuthDialog }) => {
                     }}>
                     Back
                 </Button>
-                <Button onClick={OpenLogin}>
+                <Button onClick={() => {
+                    handleLoginVisitor();
+                }}>
                     Login
                 </Button>
             </DialogActions>
