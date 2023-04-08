837472e43556fe70be53f2ae164cc35ca76fa92d
Mamad Mamadian <tocivey603@mustbeit.com>
2023-04-08 17:58:40 +0330

    remove origin from cors

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
-// app.use(cors());
+app.use(cors());
 
 app.use("/", routes);
 app.use("/admin", routes);
